c21e27e8c317166cadeb6e606b319511178cc89e
GDDMN <apawkin@gmail.com>
2023-11-29 03:05:40 +0300

    add asteroids spawner

--- Assets/Scripts/Providers/AsteroidsSpawnerProvider.cs
+++ Assets/Scripts/Providers/AsteroidsSpawnerProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Voody.UniLeo;
+using UnityEngine;
 
 public class AsteroidsSpawnerProvider : MonoProvider<AsteroidsSpawnerComponent>
 {
-
+  public Transform AscteroidsPool;
+  public GameObject AsteroidPrefab;
 }
